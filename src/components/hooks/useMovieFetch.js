c37b6340dd21a84328a3fd894fa98181de75e490
noobsaibot7 <francotechk@gmail.com>
2020-02-04 10:40:28 +0100

    fixed local storage save  differences

--- src/components/hooks/useMovieFetch.js
+++ src/components/hooks/useMovieFetch.js
@@ -31,7 +31,7 @@ export const useMovieFetch = movieId => {
 
   useEffect(() => {
     if (
-      movieId in localStorage &&
+      [movieId] in localStorage &&
       JSON.parse(localStorage[movieId]).length > 0
     ) {
       setMovie(JSON.parse(localStorage[movieId]));
